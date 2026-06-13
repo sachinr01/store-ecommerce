4d9c3fa21eed8872d59fec7855e5dd3222dbdbae
Sushant Namurte <sushantnamurte1604@gmail.com>
2026-06-13 16:35:10 +0530

    favicon change

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -48,8 +48,8 @@ export default function RootLayout({
         <link rel="stylesheet" type="text/css" href="/js/specific/revolution-slider/css/settings.css" media="screen" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,400,600,700,300" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400&display=swap" rel="stylesheet" />
-        <link rel="shortcut icon" href="/images/favicon.jpeg" type="image/jpeg" />
-        <link rel="apple-touch-icon" href="/images/favicon.jpeg" type="image/jpeg" />
+        <link rel="shortcut icon" href="/images/favicon1.jpeg" type="image/jpeg" />
+        <link rel="apple-touch-icon" href="/images/favicon1.jpeg" type="image/jpeg" />
         <Script
   src="https://checkout.shiprocket.in/js/sdk.js"
   strategy="beforeInteractive"
